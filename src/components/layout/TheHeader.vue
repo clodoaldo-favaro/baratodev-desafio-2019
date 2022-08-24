5fecc84eca1314f503278ef01d8e157a5d54da86
Clodoaldo Favaro <clodoaldofavaro@proton.me>
2022-08-23 21:57:40 -0300

    feat: cria header

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+<header>
+	<h1>Barato Coletivo</h1>
+</header>
+</template>
+
+<style scoped>
+
+header {
+	width: 100%;
+	height: 5rem;
+	color: var(--green-300);
+	background-color: var(--green-500);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+</style>
