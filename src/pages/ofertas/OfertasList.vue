3dbdb731e96f86cc7fa9c3ef4ace10701f9e51ca
Clodoaldo Favaro <clodoaldofavaro@proton.me>
2022-08-24 21:25:22 -0300

    inclui tailwind

--- src/pages/ofertas/OfertasList.vue
+++ src/pages/ofertas/OfertasList.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container">
-		<h2>Ofertas</h2>
+		<h2 class="text-xl font-bold">Ofertas</h2>
 	</div>
 </template>
 
@@ -9,6 +9,89 @@ export default {
 	data() {
 		return {
 			ofertas: [
+				{
+					id: 1, 
+					empresa: 'Hotel Long Beach',
+					descricao: 'Canoa Quebrada: 2 Diárias em quarto Luxo ou Standard a partir de R$350',
+					preco: 350.00,
+					categoria: 'hotéis',
+					imagens: [
+						'https://static.baratocoletivo.com.br/2022/0530/oferta_16539447543936_1.jpeg',
+						'https://static.baratocoletivo.com.br/2022/0530/g_c1b7b461f4.jpg',
+						'https://static.baratocoletivo.com.br/2022/0530/g_4d17226fc1.jpg',
+						'https://static.baratocoletivo.com.br/2022/0530/g_d08b2c73ae.jpg'
+					]
+				},
+				{
+					id: 2, 
+					empresa: 'Acqua Beach Cumbuco',
+					descricao: 'Cumbuco: 2 Passaportes para o Parque Aquático + Almoço!',
+					preco: 109.90,
+					categoria: 'entretenimento',
+					imagens: [
+
+					]
+				},
+				{
+					id: 1, 
+					empresa: 'Hotel Long Beach',
+					descricao: '2 diárias em quarto com vista para o mar',
+					preco: 100.00,
+					categoria: 'hotéis',
+					imagens: [
+
+					]
+				},
+				{
+					id: 1, 
+					empresa: 'Hotel Long Beach',
+					descricao: '2 diárias em quarto com vista para o mar',
+					preco: 100.00,
+					categoria: 'hotéis',
+					imagens: [
+
+					]
+				},
+				{
+					id: 1, 
+					empresa: 'Hotel Long Beach',
+					descricao: '2 diárias em quarto com vista para o mar',
+					preco: 100.00,
+					categoria: 'hotéis',
+					imagens: [
+
+					]
+				},
+				{
+					id: 1, 
+					empresa: 'Hotel Long Beach',
+					descricao: '2 diárias em quarto com vista para o mar',
+					preco: 100.00,
+					categoria: 'hotéis',
+					imagens: [
+
+					]
+				},
+				{
+					id: 1, 
+					empresa: 'Hotel Long Beach',
+					descricao: '2 diárias em quarto com vista para o mar',
+					preco: 100.00,
+					categoria: 'hotéis',
+					imagens: [
+
+					]
+				},
+				{
+					id: 1, 
+					empresa: 'Hotel Long Beach',
+					descricao: '2 diárias em quarto com vista para o mar',
+					preco: 100.00,
+					categoria: 'hotéis',
+					imagens: [
+
+					]
+				},
 				{
 					id: 1, 
 					empresa: 'Hotel Long Beach',
