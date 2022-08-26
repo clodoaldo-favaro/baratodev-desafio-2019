af3c6ff5e5e9601661aebe395e53e1b510e07d9c
Clodoaldo Favaro <clodoaldofavaro@proton.me>
2022-08-26 00:01:29 -0300

    estiliza botao

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,29 @@
+<template>
+	<button class="w-auto h-12 py-2 px-12 rounded-lg font-bold" :class="buttonClass">{{ title }}</button>
+</template>
+
+<script>
+export default {
+	props: ['title', 'type'],
+	computed: {
+		buttonClass() {
+			return this.type === 'confirm' ? 'confirm' : 'cancel';
+		}
+	}
+}
+</script>
+
+<style scoped>
+	.confirm {
+		background-color: var(--black-500);
+		color: var(--white-500);
+	}
+
+	.confirm:hover {
+		background-color: #2C2B26;
+	}
+	.cancel {
+		background-color: var(--white-500);
+		color: var(--black-500);
+	}
+</style>
--- src/components/ofertas/OfertaItem.vue
+++ src/components/ofertas/OfertaItem.vue
@@ -4,12 +4,16 @@
 		<h3 class="text-lg font-bold mt-2">{{ oferta.empresa }}</h3>
 		<p> {{ oferta.descricao }}</p>
 		<p> {{ preco }}</p>
-		<button>Comprar</button>
+		<base-button type="confirm" title="Comprar"></base-button>
 	</a>
 </template>
 
 <script>
+import BaseButton from '../UI/BaseButton.vue';
 export default {
+	components: {
+		BaseButton
+	},
 	props: ['oferta'],
 	computed: {
 		preco() {
