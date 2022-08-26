c50be2a2f649e062f6afe2a9898791171f88582b
Clodoaldo Favaro <clodoaldofavaro@proton.me>
2022-08-25 23:38:29 -0300

    listagem mais ou menos

--- src/pages/ofertas/OfertasList.vue
+++ src/pages/ofertas/OfertasList.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="main-container">
 		<section v-for="(ofertasPorCategoria, categoria) in ofertas" :key="categoria">
-			<h2 class="text-2xl font-bold capitalize mb-2 mt-4">{{ categoria }}</h2>
+			<h2 class="text-2xl font-bold capitalize mb-4 mt-4">{{ categoria }}</h2>
 			<ul class="flex flex-wrap justify-between">
-				<li class="" v-for="oferta in ofertasPorCategoria" :key="oferta.id">
+				<li class="min-w-364 my-4" v-for="oferta in ofertasPorCategoria" :key="oferta.id">
 					<oferta-item :oferta="oferta"></oferta-item>
 				</li>
 			</ul>
@@ -118,7 +118,11 @@ export default {
 					preco: 50.00,
 					categoria: 'gastronomia',
 					imagens: [
-
+						'https://static.baratocoletivo.com.br/2022/0607/oferta_16546192043752_1.jpg',
+						'https://static.baratocoletivo.com.br/2022/0607/g_865f0acb38.jpg',
+						'https://static.baratocoletivo.com.br/2022/0607/g_d7c0f6763a.jpg',
+						'https://static.baratocoletivo.com.br/2022/0607/g_e0487c0574.jpg',
+						'https://static.baratocoletivo.com.br/2022/0607/g_7f6cedea9c.jpg'
 					]
 				},
 				{
@@ -128,7 +132,11 @@ export default {
 					preco: 99.90,
 					categoria: 'gastronomia',
 					imagens: [
-
+						'https://static.baratocoletivo.com.br/2021/0715/oferta_16263784448290_destaque.jpg',
+						'https://static.baratocoletivo.com.br/2021/0715/g_b9d3d1dd2a.jpg',
+						'https://static.baratocoletivo.com.br/2021/0715/g_92f7efceb9.jpg',
+						'https://static.baratocoletivo.com.br/2021/0715/g_42690dee83.jpg',
+						'https://static.baratocoletivo.com.br/2021/0715/g_c8a0b8d4d6.jpg'
 					]
 				},
 				{
@@ -138,17 +146,25 @@ export default {
 					preco: 299.00,
 					categoria: 'hoteis',
 					imagens: [
-
+						'https://static.baratocoletivo.com.br/2022/0823/oferta_16612642352199_oferta_15819610059735_destaque.jpg',
+						'https://static.baratocoletivo.com.br/2022/0823/g_bf256c8d0d.jpg',
+						'https://static.baratocoletivo.com.br/2022/0823/g_beadbe4012.jpg',
+						'https://static.baratocoletivo.com.br/2022/0823/g_43c9bd3653.jpg',
+						'https://static.baratocoletivo.com.br/2022/0823/g_dc8e51a41c.jpg'
 					]
 				},
 				{
 					id: 10, 
-					empresa: 'Barraca Atlantidz',
-					descricao: '4 opções prato + Pulseira p/ o parque Aquático a partir de R$99,90!',
-					preco: 99.90,
+					empresa: 'La Pasta Ristorante - Parquelândia',
+					descricao: 'Rodízio Completo de Massas, Pizzas e Esfirras de R$49,90 por R$42,90',
+					preco: 42.90,
 					categoria: 'gastronomia',
 					imagens: [
-
+						'https://static.baratocoletivo.com.br/2022/0620/oferta_16557464339289_capadddd.jpg',
+						'https://static.baratocoletivo.com.br/2022/0620/g_c1d7ec5fbd.jpg',
+						'https://static.baratocoletivo.com.br/2022/0620/g_e3c8f8b89d.jpg',
+						'https://static.baratocoletivo.com.br/2022/0620/g_47bf49c978.jpg',
+						'https://static.baratocoletivo.com.br/2022/0620/g_0918bbfede.jpg'
 					]
 				},
 				{
@@ -158,7 +174,11 @@ export default {
 					preco: 12.90,
 					categoria: 'entretenimento',
 					imagens: [
-
+						'https://static.baratocoletivo.com.br/2022/0802/oferta_16594625705760_apoena.jpg',
+						'https://static.baratocoletivo.com.br/2022/0607/g_29c6ae5804.jpg',
+						'https://static.baratocoletivo.com.br/2022/0607/g_f0de30fa08.jpg',
+						'https://static.baratocoletivo.com.br/2022/0607/g_9e81208c6a.jpg',
+						'https://static.baratocoletivo.com.br/2022/0607/g_451d0e5e6f.jpg'
 					]
 				},
 				{
@@ -168,7 +188,25 @@ export default {
 					preco: 52.90,
 					categoria: 'entretenimento',
 					imagens: [
-
+						'https://static.baratocoletivo.com.br/2022/0805/oferta_16597237586014_1.jpg',
+						'https://static.baratocoletivo.com.br/2022/0805/g_c89d70916f.jpg',
+						'https://static.baratocoletivo.com.br/2022/0805/g_8bd183ed95.jpg',
+						'https://static.baratocoletivo.com.br/2022/0805/g_6a2a52da58.jpg',
+						'https://static.baratocoletivo.com.br/2022/0805/g_ad19ad9fcf.jpg'
+					]
+				},
+				{
+					id: 13,
+					empresa: 'Estilo Praia',
+					descricao: 'Tilápia completa + 02 Águas de Coco + 01 pulseira de acesso à piscina por R$69,90',
+					preco: 69.90,
+					categoria: 'gastronomia',
+					imagens: [
+						'https://static.baratocoletivo.com.br/2022/0819/oferta_16609315185658_Novo%20Projetooo%20%281%29.jpg',
+						'https://static.baratocoletivo.com.br/2022/0819/g_0103e87a70.jpg',
+						'https://static.baratocoletivo.com.br/2022/0819/g_84350f0c3c.jpg',
+						'https://static.baratocoletivo.com.br/2022/0819/g_f8af51a0a6.jpg',
+						'https://static.baratocoletivo.com.br/2022/0819/g_dbb47d105b.jpg'
 					]
 				}
 			];
@@ -196,7 +234,6 @@ export default {
 	}
 
 	li {
-		min-width: 320px;
-		max-width: 20%;
+		max-width: 22%;
 	}
 </style>
