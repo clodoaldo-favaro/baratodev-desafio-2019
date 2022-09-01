40b11a7976906400f733b111f01d30935b66af3f
Clodoaldo Favaro <clodoaldofavaro@proton.me>
2022-08-31 23:48:16 -0300

    wip: visualizando items no carrinho

---
+++ src/components/carrinho/CarrinhoItemCard.vue
@@ -0,0 +1,22 @@
+<template>
+	<li class="mb-12">
+		<p>{{ item.descricao }}</p>
+		<p>{{ preco }}</p>
+		<p>{{ item.quantidade }}</p>
+		<p> {{ total }}</p>
+	</li>
+</template>
+
+<script>
+	export default {
+		props: ['item'],
+		computed: {
+			preco() {
+				return 'R$ ' + this.item.preco.toFixed(2); 
+			},
+			total() {
+				return 'R$ ' + (this.item.quantidade * this.item.preco).toFixed(2); 
+			}
+		}
+	}
+</script>
--- src/pages/carrinho/CarrinhoCompras.vue
+++ src/pages/carrinho/CarrinhoCompras.vue
@@ -1,3 +1,34 @@
 <template>
-	<h1>Carrinho de compras</h1>
+	<h1 class="text-2xl font-bold mb-12">Carrinho de compras</h1>
+	<ul>
+		<carrinho-item-card v-for="item in itemsCarrinho" :item="item"></carrinho-item-card>
+	</ul>
 </template>
+
+<script>
+import CarrinhoItemCard from '../../components/carrinho/CarrinhoItemCard.vue';
+export default {
+	components: {
+		CarrinhoItemCard
+	},
+	computed: {
+		itemsCarrinho() {
+			const itemsAgrupados = {};
+			const items = this.$store.getters['carrinho/items'];
+
+			items.forEach((item) => {
+				debugger;
+				if (!(item.id in itemsAgrupados)) {
+					itemsAgrupados[item.id] = {
+						quantidade: 0,
+						...item
+					}
+				}
+				itemsAgrupados[item.id]['quantidade']++;
+			});
+			return itemsAgrupados;
+		}
+	}
+}
+</script>
+
