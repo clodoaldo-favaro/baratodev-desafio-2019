bb49f991700003bf0f700f548e0752d49e9e1d99
Clodoaldo Favaro <clodoaldofavaro@proton.me>
2022-09-22 22:39:40 -0300

    wip: tela de detalhe de oferta

--- src/pages/ofertas/OfertaDetail.vue
+++ src/pages/ofertas/OfertaDetail.vue
@@ -1,14 +1,40 @@
 <template>
-    <h2>Ofertas</h2>
-    {{ ofertaId }}
+    <div class="flex flex-col sm:flex-row sm:justify-between">
+        <div class="flex flex-col sm:w-3/6">
+            <div class="selected-image mb-2">
+                <img :src="oferta.imagens[0]" alt="">
+            </div>
+            <div class="thumbnails flex flex-row justify-between">
+                <img v-for="imagem in oferta.imagens.slice(1, 3)" :key="imagem" :src="imagem" alt="">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                oferta: {}
+            }
+        },
         computed: {
             ofertaId() {
                 return this.$route.params['id'];
             }
+        },
+        methods: {
+            getOfertaById() {
+                this.oferta = this.$store.getters['ofertas/getOfertaById'](this.ofertaId);
+                console.log(this.oferta);
+            }
+        },
+        created() {
+            this.getOfertaById();
         }
     }
 </script>
+
+<style scoped>
+    
+</style>
--- src/store/modules/ofertas/getters.js
+++ src/store/modules/ofertas/getters.js
@@ -1,5 +1,9 @@
 export default {
 	ofertas(state) {
 		return state.ofertas;
+	},
+	getOfertaById: (state) => (id) => {
+		const oferta = state.ofertas.find(oferta => oferta.id == id);
+		return oferta;
 	}
 }
